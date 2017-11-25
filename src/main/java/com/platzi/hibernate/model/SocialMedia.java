07b24fb1890710e505803b03ac2dea48db7a12a2
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2017-11-25 18:04:38 -0300

    update/add - adding package to handle database with class and adding class for socialmedia.

---
+++ src/main/java/com/platzi/hibernate/model/SocialMedia.java
@@ -0,0 +1,9 @@
+package com.platzi.hibernate.model;
+
+import java.io.Serializable;
+
+public class SocialMedia implements Serializable {
+	private Long idSocialMedia;
+	private String name;
+	private String icon;
+}
