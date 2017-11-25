d17c73b503806df3229e4dc3c1150c8248ef3079
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2017-11-25 18:30:18 -0300

    update/add - adding class teacher_social_media with getters and setters, maping database with db columns.

--- src/main/java/com/platzi/hibernate/model/TeacherSocialMedia.java
+++ src/main/java/com/platzi/hibernate/model/TeacherSocialMedia.java
@@ -9,6 +9,50 @@ import javax.persistence.GenerationType;
 import javax.persistence.Table;
 import javax.persistence.Id;
 
-public class TeacherSocialMedia {
-
+@Entity
+@Table(name="teacher_social_media")
+public class TeacherSocialMedia implements Serializable {
+	@Id
+	@Column(name="id_teacher_social_media")
+	@GeneratedValue(strategy=GenerationType.IDENTITY) //Esto significa que este campo es un campo identidad autoincremental
+	private Long IdTeacherSocialMedia;
+	private Teacher teacher;
+	private SocialMedia socialMedia;
+	private String nickname;
+	
+	public TeacherSocialMedia() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public TeacherSocialMedia(Teacher teacher, SocialMedia socialMedia, String nickname) {
+		super();
+		this.teacher = teacher;
+		this.socialMedia = socialMedia;
+		this.nickname = nickname;
+	}
+	public Long getIdTeacherSocialMedia() {
+		return IdTeacherSocialMedia;
+	}
+	public void setIdTeacherSocialMedia(Long idTeacherSocialMedia) {
+		IdTeacherSocialMedia = idTeacherSocialMedia;
+	}
+	public Teacher getTeacher() {
+		return teacher;
+	}
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
+	public SocialMedia getSocialMedia() {
+		return socialMedia;
+	}
+	public void setSocialMedia(SocialMedia socialMedia) {
+		this.socialMedia = socialMedia;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
 }
