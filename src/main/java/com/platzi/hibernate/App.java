db8b41447954474bacd122b8d54abc9b8a3c8469
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2017-11-25 19:39:32 -0300

    update/add - adding in app class, the import of tables to begin insert and get data, also adding into cfg project, the mapping of databases.

--- src/main/java/com/platzi/hibernate/App.java
+++ src/main/java/com/platzi/hibernate/App.java
@@ -1,5 +1,11 @@
 package com.platzi.hibernate;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
+import com.platzi.hibernate.model.Course;
+import com.platzi.hibernate.model.SocialMedia;
+import com.platzi.hibernate.model.Teacher;
 /**
  * Hello world!
  *
@@ -9,5 +15,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        SessionFactory sessionFactory;
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        
+        
+        
     }
 }
